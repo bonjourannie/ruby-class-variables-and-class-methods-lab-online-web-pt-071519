8fd835c00442c6968fe6c1ea7be17a471a282c8d
Annie Truex <artruex@gmail.com>
2019-08-08 19:50:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   
   def self.artist_count
     artist_count = {}
-    artist_count.each do |artist|
+    @@artist_count.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1 
       else 
